3d7e4f5580ea6b1c49e9e8400b01709056a17af9
Pink727 <doc72789@gmail.com>
2024-12-08 16:53:49 -0800

    Add default messages for missing candidate details in CandidateCard component

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Candidate from '../interfaces/Candidate.interface';
+import '../styles/CandidateCard.css';
 
 interface CandidateCardProps {
   candidate: Candidate;
@@ -7,15 +8,15 @@ interface CandidateCardProps {
 
 const CandidateCard: React.FC<CandidateCardProps> = ({ candidate }) => {
   return (
-    <li key={candidate.id}>
+    <li className="candidate-card" key={candidate.id}>
       <img src={candidate.avatar_url} alt={candidate.login} width="50" />
       <a href={candidate.html_url} target="_blank" rel="noopener noreferrer">
         {candidate.login}
       </a>
-      <p>Name: {candidate.name}</p>
-      <p>Location: {candidate.location}</p>
-      <p>Email: {candidate.email}</p>
-      <p>Company: {candidate.company}</p>
+      <p>Name: {candidate.name || 'user has not added that info'}</p>
+      <p>Location: {candidate.location || 'user has not added that info'}</p>
+      <p>Email: {candidate.email || 'user has not added that info'}</p>
+      <p>Company: {candidate.company || 'user has not added that info'}</p>
     </li>
   );
 };
