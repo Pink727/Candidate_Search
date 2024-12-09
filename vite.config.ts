a1be204536f9bc3880bcbc5c1598b4a1cb48246f
Pink727 <doc72789@gmail.com>
2024-12-09 12:53:55 -0800

    Replace public/index.html with new index.html; update Vite configuration to specify env directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './env',
   plugins: [react()],
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-  },
 });
